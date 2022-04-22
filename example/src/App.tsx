040b61bc7d689b9e3658242a21884fc0b9c4a33b
Jannis Morgenstern <jannis.morgenstern@titanom.com>
2022-04-22 11:30:33 +0200

    feat: add e2e tests for `useDimensions`

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import UseDimensionsRoute from './routes/use-dimensions';
+import UseResizeObserverRoute from './routes/use-resize-observer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/use-dimensions" element={<UseDimensionsRoute />} />
+        <Route
+          path="/use-resize-observer"
+          element={<UseResizeObserverRoute />}
+        />
       </Routes>
     </div>
   );
