762ebc71dac00fc78c2e36f2debf06bd648a4fe5
Jannis Morgenstern <jannis.morgenstern@titanom.com>
2022-04-22 06:56:20 +0200

    chore: migrate '@testing-library/react-hooks' to '@testing-library/react'

--- tests/hooks/use-render-count.test.ts
+++ tests/hooks/use-render-count.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import useRenderCount from '../../src/hooks/use-render-count';
 
 describe('useRenderCount', () => {
